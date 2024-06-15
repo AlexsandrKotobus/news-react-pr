fec0c9e5b1b4191059db241c1d96feb3f7048149
Julia <135132600+AlexsandrKotobus@users.noreply.github.com>
2024-06-15 11:33:52 +0300

    first news page

---
+++ src/api/apiNews.js
@@ -0,0 +1,29 @@
+﻿import axios from "axios";
+
+const BASE_URL=import.meta.env.VITE_NEWS_BASE_API_URL
+const API_KEY=import.meta.env.VITE_NEWS_API_KEY
+
+// VITE_NEWS_API_KEY=CeBOB1LsL1SZPFQRPqyKH0ZSGr8OYbbgU8NN6GjivfKCF3Gx
+// VITE_NEWS_BASE_API_URL=https://api.currentsapi.services/v1/
+
+// // const BASE_URL= api.currentsapi.services/v1/
+// const API_KEY='CeBOB1LsL1SZPFQRPqyKH0ZSGr8OYbbgU8NN6GjivfKCF3Gx'
+// VITE_NEWS_API_KEY
+console.log(import.meta.env.VITE_NEWS_BASE_API_URL) // 
+console.log(import.meta.env.VITE_NEWS_API_KEY) // undefined
+console.log('BASE_URL', `${BASE_URL}latest-news`)
+
+export const getNews= async () => {
+    try{
+        const response = await axios.get(`${BASE_URL}latest-news`, {
+            params: {
+                apiKey: API_KEY
+            }
+        })
+        console.log(response.data)
+        return response.data;
+    }
+    catch(error){
+        console.log(error);
+    }
+}
