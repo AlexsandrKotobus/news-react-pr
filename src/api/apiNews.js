c226d09882ffe7df33f062916f8415edfd2ac3ec
Julia <135132600+AlexsandrKotobus@users.noreply.github.com>
2024-06-23 23:05:54 +0300

    add pagination, change api endpoint

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -9,15 +9,17 @@ const API_KEY=import.meta.env.VITE_NEWS_API_KEY
 // // const BASE_URL= api.currentsapi.services/v1/
 // const API_KEY='CeBOB1LsL1SZPFQRPqyKH0ZSGr8OYbbgU8NN6GjivfKCF3Gx'
 // VITE_NEWS_API_KEY
-console.log(import.meta.env.VITE_NEWS_BASE_API_URL) // 
-console.log(import.meta.env.VITE_NEWS_API_KEY) // undefined
-console.log('BASE_URL', `${BASE_URL}latest-news`)
+// console.log(import.meta.env.VITE_NEWS_BASE_API_URL) // 
+// console.log(import.meta.env.VITE_NEWS_API_KEY) // undefined
+// console.log('BASE_URL', `${BASE_URL}latest-news`)
 
-export const getNews= async () => {
+export const getNews= async (page_number=1, page_size=10) => {
     try{
-        const response = await axios.get(`${BASE_URL}latest-news`, {
+        const response = await axios.get(`${BASE_URL}search`, {
             params: {
-                apiKey: API_KEY
+                apiKey: API_KEY,
+                page_number,
+                page_size,
             }
         })
         console.log(response.data)
