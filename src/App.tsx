1e7300d901808ba38748874d9cce95f5992c782f
Julia <135132600+AlexsandrKotobus@users.noreply.github.com>
2024-09-18 16:00:48 +0300

    add rtk query and redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Header from './components/Header/Header'
 import Main from './pages/Main/Main'
 import { useTheme } from './context/ThemeContext'
+import { useAppSelector } from './store';
 
 
 function App() {
 
  const {isDark} = useTheme();
+ const news = useAppSelector(state => state.news)        //для проверки
+ console.log(news)                                        //для проверки
 //  console.log("App", {isDark})
   return (
        <div className={`app ${isDark ? 'dark' : 'light'}`}>
