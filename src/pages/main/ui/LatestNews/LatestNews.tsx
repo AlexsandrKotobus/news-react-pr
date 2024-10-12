a117416fa1378372c84c834656831183e10997be
Julia <135132600+AlexsandrKotobus@users.noreply.github.com>
2024-10-13 00:41:52 +0300

    add news page and react router

--- src/pages/main/ui/LatestNews/LatestNews.tsx
+++ src/pages/main/ui/LatestNews/LatestNews.tsx
@@ -2,18 +2,30 @@
 import { useGetLatestNewsQuery } from '@/entities/news/api/newsApi';
 import {  NewsList } from '@/widgets/news/';
 import styles from './styles.module.css'
+import { INews } from '@/entities/news';
+import { setCurrentNews } from '@/entities/news/model/newsSlices';
+import { useAppDispatch } from '@/app/appStore';
+import { useNavigate } from 'react-router-dom';
 
 const LatestNews = () => {
-   
     const { data, isLoading} = useGetLatestNewsQuery(null);
-    // console.log("LatestNews  ", data)
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const navigateTo = (news: INews) => {
+        dispatch(setCurrentNews(news));
+        navigate(`/news/${news.id}`);
+    }
+    
     return (
         <section className={styles.section}>
             <NewsList 
             type='banner' 
             direction ='row' 
             news={data && data.news} 
-            isLoading={isLoading} />
+            isLoading={isLoading} 
+            viewNewsSlot={(news: INews) => <p onClick={() => navigateTo(news)}>view more...</p>}
+            />
         </section>
     );
 }
--- src/widgets/header/ui/Header/Header.tsx
+++ src/widgets/header/ui/Header/Header.tsx
@@ -2,17 +2,20 @@ import { useTheme } from '@/app/providers/ThemeProvider';
 import { formatDate } from '@/shared/helpers/formatDate';
 import styles from './styles.module.css'
 import ThemeButton from '@/features/theme/ui/ThemeButton/ThemeButton';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const {isDark} = useTheme()
     return (
-        <header className={`${styles.header} ${isDark ? styles.dark : styles.light}`}>
-          <div className={styles.info}>
+      <header className={`${styles.header} ${isDark ? styles.dark : styles.light}`}>
+        <div className={styles.info}>
+          <Link to={'/'}>
             <h1 className={styles.title}>News React</h1>
-            <p className={styles.date}>{formatDate(new Date())}</p>
-          </div>
-          <ThemeButton />
-        </header>
+          </Link>
+          <p className={styles.date}>{formatDate(new Date())}</p>
+        </div>
+        <ThemeButton />
+      </header>
     );
 }
 
